3aa0903d9833cbb19c1916dbfd3e75a9ff8f9940
Сергей <sergey_sorokin.1580@mail.ru>
2021-09-29 01:57:41 +0700

    #9

--- backend/foodgram_api/settings.py
+++ backend/foodgram_api/settings.py
@@ -103,7 +103,7 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.IsAuthenticatedOrReadOnly',
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
