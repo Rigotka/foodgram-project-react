c25eda9845b79f8562a4f614c7396421dd0d4d79
Сергей <sergey_sorokin.1580@mail.ru>
2021-09-29 01:39:18 +0700

    #9

--- backend/users/views.py
+++ backend/users/views.py
@@ -3,7 +3,7 @@ from djoser.views import UserViewSet
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
 from .models import Subscription, User
@@ -13,7 +13,7 @@ from .serializers import SubscriptionSerializer, UserSerializer
 class CustomUserViewSet(UserViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     pagination_class = PageNumberPagination
 
     @action(methods=['get', 'delete'], detail=True,
