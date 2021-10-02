0153f96335129c9dd00b7103984ec4f82af37a76
Сергей <sergey_sorokin.1580@mail.ru>
2021-10-03 06:36:13 +0700

    help

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -4,7 +4,7 @@ from .models import Recipe, Tag, Ingredient
 
 
 class IngredientFilter(filters.FilterSet):
-    name = filters.CharFilter(field_name="name", lookup_expr='startswith')
+    name = filters.CharFilter(field_name="name", lookup_expr='iexact')
 
     class Meta:
         model = Ingredient
