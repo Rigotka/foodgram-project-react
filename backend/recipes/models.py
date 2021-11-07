5127d24be444da2ba22e42c6d559596b497cdf1e
Rigotka <sergey_sorokin.1580@mail.ru>
2021-11-08 01:00:38 +0700

    fix

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -17,12 +17,12 @@ class RecipeQueryset(models.QuerySet):
             )
         return self.annotate(is_favorited=Exists(
             Favorite.objects.filter(
-                user=user, recipe_id=OuterRef('pk')
+                user=user, recipe_id=OuterRef('id')
             )
         ),
             is_in_shopping_cart=Exists(
                 ShoppingCart.objects.filter(
-                    user=user, recipe_id=OuterRef('pk')
+                    user=user, recipe_id=OuterRef('id')
                 )
         )
         )
