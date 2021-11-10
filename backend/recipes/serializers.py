ee396657f9724ceda2fcb2d5926d6ecf9cdb5426
Rigotka <sergey_sorokin.1580@mail.ru>
2021-11-10 10:02:08 +0700

    final!

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -56,6 +56,20 @@ class ShowRecipeSerializer(serializers.ModelSerializer):
         qs = IngredientInRecipe.objects.filter(recipe=recipe)
         return IngredientInRecipeSerializer(qs, many=True).data
 
+    def get_is_favorited(self, obj):
+        request = self.context.get('request')
+        if request is None or request.user.is_anonymous:
+            return False
+        user = request.user
+        return Favorite.objects.filter(recipe=obj, user=user).exists()
+
+    def get_is_in_shopping_cart(self, obj):
+        request = self.context.get('request')
+        if request is None or request.user.is_anonymous:
+            return False
+        user = request.user
+        return ShoppingCart.objects.filter(recipe=obj, user=user).exists()
+
 
 class AddIngredientToRecipeSerializer(serializers.ModelSerializer):
     id = serializers.PrimaryKeyRelatedField(
