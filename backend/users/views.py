0d5d9384f9f39f6fff8e0cf376e80f8e175376c2
Rigotka <sergey_sorokin.1580@mail.ru>
2021-11-11 11:09:48 +0700

    final

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,7 @@
 from os import execlp
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework import permissions
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
@@ -14,50 +14,13 @@ from recipes.permissions import IsAuthorOrReadOnly
 
 from .models import Subscription, User
 from .paginator import VariablePageSizePaginator
-from .serializers import SubscriptionSerializer, UserSerializer
+from .serializers import SubscriptionSerializer, UserSerializer, ShowSubscriptionSerializer
 
 
 class CustomUserViewSet(UserViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthorOrReadOnly]
-
-#     @action(methods=['get', 'delete'], detail=True,
-#             permission_classes=[IsAuthenticated])
-#     def subscribe(self, request, id=None):
-#         user = self.request.user
-#         author = get_object_or_404(User, id=id)
-
-#         if request.method == 'GET':
-#             data = {
-#                 'user': user.id,
-#                 'author': author.id
-#             }
-#             serializer = SubscriptionSerializer(data=data, context={'request': request})
-#             if not serializer.is_valid():
-#                 return Response(
-#                     serializer.errors,
-#                     status=status.HTTP_400_BAD_REQUEST
-#                 )
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-#         obj = get_object_or_404(Subscription, user=user, author=author)
-#         if obj:
-#             obj.delete()
-#             return Response(status=status.HTTP_204_NO_CONTENT)
-#         return Response({"error": "Вы не подписаны на этого пользователя"}, status=status.HTTP_400_BAD_REQUEST)
-
-#     @action(detail=False)
-#     def subscriptions(self, request):
-#         user = self.request.user
-#         users = User.objects.filter(subscription__user=user)
-#         paginator = PageNumberPagination()
-#         paginator.page_size = 3
-#         result_page = paginator.paginate_queryset(users, request)
-#         serializer = SubscriptionSerializer(result_page, many=True,
-#                                             context={'request': request})
-#         return paginator.get_paginated_response(serializer.data)
 
 
 class SubscribeViewSet(APIView):
@@ -87,3 +50,18 @@ class SubscribeViewSet(APIView):
         author = get_object_or_404(User, id=author_id)
         get_object_or_404(Subscription, user=user, author=author).delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ListSubscribeViewSet(generics.ListAPIView):
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated, ]
+    serializer_class = ShowSubscriptionSerializer
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context.update({'request': self.request})
+        return context
+
+    def get_queryset(self):
+        user = self.request.user
+        return User.objects.filter(following__user=user)
