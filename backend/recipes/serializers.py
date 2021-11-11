ecf80e7eb1f57a1258bf50abc8ca44ddfa228d42
Rigotka <sergey_sorokin.1580@mail.ru>
2021-11-11 21:44:59 +0700

    finalgit add --allgit add --allgit add --allgit add --allgit add --all

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -86,13 +86,13 @@ class RecordRecipeSerializer(serializers.ModelSerializer):
 
     def create_bulk_ingredients(self, recipe, ingredients_data):
         print(ingredients_data)
-        IngredientInRecipe.objects.bulk_create([
-            IngredientInRecipe(
-                ingredient=ingredient['ingredient'],
-                recipe=recipe,
-                amount=ingredient['amount']
-            ) for ingredient in ingredients_data
-        ])
+        # IngredientInRecipe.objects.bulk_create([
+        #     IngredientInRecipe(
+        #         ingredient=ingredient['ingredient'],
+        #         recipe=recipe,
+        #         amount=ingredient['amount']
+        #     ) for ingredient in ingredients_data
+        # ])
 
     @transaction.atomic
     def create(self, validated_data):
