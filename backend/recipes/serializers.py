6b1832fd3576d8fbef1a8314a2027b34ad8a99db
Rigotka <sergey_sorokin.1580@mail.ru>
2021-11-11 19:58:44 +0700

    finalgit add --allgit add --allgit add --allgit add --allgit add --all

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -84,6 +84,42 @@ class RecordRecipeSerializer(serializers.ModelSerializer):
         fields = ('id', 'ingredients', 'name', 'tags',
                   'cooking_time', 'text', 'image', 'author',)
 
+    def create_bulk_ingredients(self, recipe, ingredients_data):
+        IngredientInRecipe.objects.bulk_create([
+            IngredientInRecipe(
+                ingredient=ingredient['ingredient'],
+                recipe=recipe,
+                amount=ingredient['amount']
+            ) for ingredient in ingredients_data
+        ])
+
+    @transaction.atomic
+    def create(self, validated_data):
+        ingredients_data = validated_data.pop('ingredients')
+        tags_data = validated_data.pop('tags')
+        author = self.context.get('request').user
+        recipe = Recipe.objects.create(author=author, **validated_data)
+        recipe.save()
+        recipe.tags.set(tags_data)
+        self.create_bulk_ingredients(recipe, ingredients_data)
+        return recipe
+
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        ingredients_data = validated_data.pop('ingredients')
+        tags_data = validated_data.pop('tags')
+        IngredientInRecipe.objects.filter(recipe=instance).delete()
+        self.create_bulk_ingredients(instance, ingredients_data)
+        instance.name = validated_data.pop('name')
+        instance.text = validated_data.pop('text')
+        if validated_data.get('image') is not None:
+            instance.image = validated_data.pop('image')
+        instance.cooking_time = validated_data.pop('cooking_time')
+        instance.save()
+        instance.tags.set(tags_data)
+        return instance
+
+
     # def validate_ingredients(self, value):
     #     ingredients = self.initial_data.get('ingredients')
     #     if not ingredients:
@@ -118,40 +154,40 @@ class RecordRecipeSerializer(serializers.ModelSerializer):
     #             ingredient=ingr_obj,
     #         )
 
-    @transaction.atomic
-    def create(self, validated_data):
-        tags = validated_data.pop('tags')
-        ingredients = validated_data.pop('ingredients')
-        author = self.context.get('request').user
-        recipe = Recipe.objects.create(author=author, **validated_data)
-        recipe.tags.set(tags)
-        self.create_ingredients(recipe, ingredients)
-        return recipe
+    # @transaction.atomic
+    # def create(self, validated_data):
+    #     tags = validated_data.pop('tags')
+    #     ingredients = validated_data.pop('ingredients')
+    #     author = self.context.get('request').user
+    #     recipe = Recipe.objects.create(author=author, **validated_data)
+    #     recipe.tags.set(tags)
+    #     self.create_ingredients(recipe, ingredients)
+    #     return recipe
 
-    @transaction.atomic
-    def update(self, instance, validated_data):
-        if 'tags' in self.initial_data:
-            tags = validated_data.pop('tags')
-            instance.tags.set(tags)
-        if 'ingredients' in self.initial_data:
-            ingredients = validated_data.pop('ingredients')
-            instance.ingredients.clear()
-            self.create_ingredients(instance, ingredients)
-        instance.name = validated_data.get(
-            'name',
-            instance.name
-        )
-        instance.text = validated_data.get(
-            'text',
-            instance.text
-        )
-        instance.cooking_time = validated_data.get(
-            'cooking_time',
-            instance.cooking_time
-        )
-        instance.image = validated_data.get('image', instance.image)
-        instance.save()
-        return instance
+    # @transaction.atomic
+    # def update(self, instance, validated_data):
+    #     if 'tags' in self.initial_data:
+    #         tags = validated_data.pop('tags')
+    #         instance.tags.set(tags)
+    #     if 'ingredients' in self.initial_data:
+    #         ingredients = validated_data.pop('ingredients')
+    #         instance.ingredients.clear()
+    #         self.create_ingredients(instance, ingredients)
+    #     instance.name = validated_data.get(
+    #         'name',
+    #         instance.name
+    #     )
+    #     instance.text = validated_data.get(
+    #         'text',
+    #         instance.text
+    #     )
+    #     instance.cooking_time = validated_data.get(
+    #         'cooking_time',
+    #         instance.cooking_time
+    #     )
+    #     instance.image = validated_data.get('image', instance.image)
+    #     instance.save()
+    #     return instance
 
 
 class FavoriteSerializer(serializers.ModelSerializer):
