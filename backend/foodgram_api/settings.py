8dd2cb129138969d300919b7efe6832a33b5a890
Rigotka <sergey_sorokin.1580@mail.ru>
2021-11-16 05:12:03 +0700

    finalfinal

--- backend/foodgram_api/settings.py
+++ backend/foodgram_api/settings.py
@@ -64,25 +64,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = { 
-
-#     'default': { 
-
-#         'ENGINE': os.environ.get('DB_ENGINE'), 
-
-#         'NAME': os.environ.get('DB_NAME'), 
-
-#         'USER': os.environ.get('POSTGRES_USER'), 
-
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'), 
-
-#         'HOST': os.environ.get('DB_HOST'), 
-
-#         'PORT': os.environ.get('DB_PORT'), 
-
-#     } 
-
-# } 
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
