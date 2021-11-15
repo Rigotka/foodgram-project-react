aa092af854454b1917c80cde57de91dce5e4e114
Rigotka <sergey_sorokin.1580@mail.ru>
2021-11-16 06:16:34 +0700

    help

--- backend/foodgram_api/settings.py
+++ backend/foodgram_api/settings.py
@@ -54,16 +54,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram_api.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('POSTGRES_USER',),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-    }
-}
+DATABASES = { 
+    'default': { 
+        'ENGINE': 'django.db.backends.postgresql', 
+        'NAME': 'postgres', 
+        'USER': 'postgres', 
+        'PASSWORD': 'postgres1580', 
+        'HOST': 'db', 
+        'PORT': '5432', 
+    } 
+} 
 
 AUTH_PASSWORD_VALIDATORS = [
     {
