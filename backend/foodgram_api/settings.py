b93da50751c15b76b6d5a1faa3d38e069eebb035
Rigotka <sergey_sorokin.1580@mail.ru>
2021-11-16 05:06:37 +0700

    finalfinal

--- backend/foodgram_api/settings.py
+++ backend/foodgram_api/settings.py
@@ -56,7 +56,7 @@ WSGI_APPLICATION = 'foodgram_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_USER'),
+        'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
